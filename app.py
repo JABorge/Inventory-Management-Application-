673c45349cb6c60367486ec82c7cbe556e80ae15
JABorge <60979174+JABorge@users.noreply.github.com>
2022-02-10 15:34:41 -0500

    Fixes to ORM

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {"Msg": "Hello World"}
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.sqlite3",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
